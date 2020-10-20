bbbd7dde6fe2f2eabbdfc5a0d3906a5d74e14918
Justice M <codenamesolutions@gmail.com>
2020-10-20 23:34:02 +0200

    added settings buttton on header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './header.scss';
+import { HiDotsVertical } from "react-icons/hi";
+
 
 
 const Header = () => {
 
     return(
     <div className='header'>
-        <a href='#' className='logo'>Tracklist</a>
-        <div className='login'>
-            <a href='#' className='login-button solid-button'>LOGIN</a>
+        <div className='left'>
+             <a href='#' className='logo'>Tracklist</a>
+        </div>
+        <div className='right'>
+            <div className='login'>
+                <a href='#' className='login-button solid-button'>LOGIN</a>
+            </div>
+            <div className='settings-btn'>
+                <HiDotsVertical className='dots' />
+            </div>
         </div>
     </div>
     );
